4c158f698ca59e4ab685cec6f94bb4fafaf922b7
shegalikaj <shegalikaj@gmail.com>
2022-12-10 13:39:53 +0100

    Update paxos_2.py
    added some changes

--- fake-paxos/paxos_2.py
+++ fake-paxos/paxos_2.py
@@ -4,25 +4,8 @@ import socket
 import struct
 import functions as fnc
 import json
-from paxos_algorithm.algorithm import Paxos
-from paxos_algorithm.paxos_participant import Proposer
-
-def mcast_receiver(hostport): # TODO : Check if this function is Bloking or not!
-    """create a multicast socket listening to the address"""
-    recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    recv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    recv_sock.bind(hostport)
-
-    mcast_group = struct.pack("4sl", socket.inet_aton(hostport[0]), socket.INADDR_ANY)
-    recv_sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mcast_group)
-    return recv_sock
-
-
-def mcast_sender():
-    """create a udp socket"""
-    send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    return send_sock
-
+from paxos_algorithm.algorithm import Components
+from paxos_algorithm.paxos_participant import Proposer, Acceptor, mcast_receiver, mcast_sender
 
 def parse_cfg(cfgpath):
     cfg = {}
@@ -36,78 +19,17 @@ def parse_cfg(cfgpath):
 
 def acceptor(config, id):
     print ('-> acceptor', id)
-    state = {}
-    r = mcast_receiver(config['acceptors'])
-    s = mcast_sender()
-    id_paxos = 0
-    instances_of_paxos= {}
-    while True:
-        msg = r.recv(2**16)
-        
-        if msg.origine == "proposer": 
-            if msg.phase == "PHASE 1A":
-                id_paxos = id_paxos + 1
-                # TODO this should removed. 
-                instance_of_paxo= Paxos(id_paxos, s, config['proposer'])
-                instances_of_paxos.update({id_paxos: instance_of_paxo})
-                instances_of_paxos.acceptor_phase_1B(msg.c_rnd, msg.id_proposer)
-                # instance_of_paxos = instances_of_paxos[msg.id_paxos]
-                
-        # m = json.loads(msg.decode())
-        # #m = json.load(msg).decode()
-        # print(m)
-        # print(type(m))
-        # print(m['id_paxos'])
-        # # fake acceptor! just forwards messages to the learner
-        # if id == 1:
-        #     # print "acceptor: sending %s to learners" % (msg)
-        #     s.sendto(msg, config['learners'])
-
+    proposer_obj = Acceptor(id, config['acceptors'], config['proposers'])
+    proposer_obj.construct()
+    proposer_obj.run()
+            
 
 
 def proposer(config, id):
     print ('-> proposer', id)
-    proposer_obj = Proposer(id).construct_proposer(config['acceptors'], config['proposers'])
-    r = proposer_obj.mcast_receiver
-    s = proposer_obj.mcast_sender 
-    
-    
-
-    # print ('-> proposer', id)
-    # r = mcast_receiver(config['proposers'])
-    # s = mcast_sender()
-    # id_paxos = 0
-    # first_part_of_c_rnd = 0
-    # instances_of_paxos= {}
-    # while True:
-        
-        
-        
-    #     msg = r.recv(2**16)
-        
-        
-    #     if msg.origine == "client": 
-    #         first_part_of_c_rnd = first_part_of_c_rnd + 1
-    #         id_paxos = id_paxos + 1
-    #         instance_of_paxo = Paxos(id_paxos, s, config['acceptors'])
-    #         instance_of_paxo.proposer_phase_1A(msg.content, id, first_part_of_c_rnd)
-    #         instances_of_paxos.update({id_paxos: instance_of_paxo})
-    #     elif msg.origine == "acceptor": 
-    #         if msg.phase == "PHASE 1B":
-    #            instance_of_paxos = instances_of_paxos[msg.id_paxos]
-    #            instance_of_paxos.proposer_phase_2A()
-    #         elif msg.phase == "PHASE 2B":
-    #             instance_of_paxos = instances_of_paxos[msg.id_paxos]
-    #             instance_of_paxos.proposer_phase_3()
-                
-              
-        # fake proposer! just forwards message to the acceptor
-        # if id == 1:
-        #     # print "proposer: sending %s to acceptors" % (msg)
-        #     m =  json.dumps({"c_rnd": (2,2), "id_paxos": 1}).encode('utf8')
-        #     s.sendto(m, config['acceptors'])
-        
-            
+    proposer_obj = Proposer (id, config['proposers'], config['acceptors'])
+    proposer_obj.construct()
+    proposer_obj.run()
         
 
 
@@ -126,8 +48,7 @@ def client(config, id):
         value = value.strip()
         print ("client: sending %s to proposers" % (value))
         s.sendto(value.encode(), config['proposers'])
-    print ('client done.')
-
+    print ('client done.')    
 
 if __name__ == '__main__':
         cfgpath = sys.argv[1]
