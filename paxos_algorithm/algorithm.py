0dc3cc6b55d132aac3fc97a431d26fc11e828f50
shegalikaj <shegalikaj@gmail.com>
2022-12-10 12:07:15 +0100

    Update algorithm.py
    added some changes

--- paxos_algorithm/algorithm.py
+++ paxos_algorithm/algorithm.py
@@ -8,30 +8,24 @@ v_rnd = None
 v_val = None
 
 
-class Paxos:
-    def __init__(self, id_paxos, socket_proposer, receivers):
-        self.c_rnd = None
-        self.c_val = None
-        self.id_paxos= id_paxos
-        self.socket_proposer = socket_proposer
-        self.receivers  = receivers
-        # self.c_rnd = None
-        self.v = None
-        self.rnd = 0
-        
+class Components:
+    def __init__(self):
+        pass
     def proposer_phase_1A(self, v, id_proposer, first_part_of_c_rnd):
-        # increase the c-round to arbitrary unique value 
-        #(id_proposer, random.randint(0,100))
-        self.c_rnd  = (first_part_of_c_rnd, id_proposer)
-        self.v = v 
-        msg= json.dumps({"c_rnd": self.c_rnd, "id_paxos": self.id_paxos, "id_proposer": id_proposer, "origine": "proposer", "phase": "PHASE 1A"}).encode('utf8')
-        self.socket_proposer.sendto(msg, self.receivers)
-        
+        # # increase the c-round to arbitrary unique value 
+        # #(id_proposer, random.randint(0,100))
+        # self.c_rnd  = (first_part_of_c_rnd, id_proposer)
+        # self.v = v 
+        # msg= json.dumps({"c_rnd": self.c_rnd, "id_paxos": self.id_paxos, "id_proposer": id_proposer, "origine": "proposer", "phase": "PHASE 1A"}).encode('utf8')
+        # self.socket_proposer.sendto(msg, self.receivers)
+        pass
+    
     def acceptor_phase_1B(self, c_rnd, s, id_proposer):
-        if c_rnd(0)> self.rnd:
-            rnd = c_rnd
-            s.sentto(receivers)
-        
+        # if c_rnd(0)> self.rnd:
+        #     rnd = c_rnd
+        #     msg= json.dumps({"rnd": self.rnd, "id_proposer": id_proposer, "origine": "acceptor", "phase": "PHASE 1B"}).encode('utf8')
+        #     s.sentto(self.receivers)
+    
         pass
     
     
@@ -46,26 +40,26 @@ class Paxos:
     
     
 
-proposers = [1,2,3]   
-messages = ["a", "b", "c"]
-id_paxos = 0
+# proposers = [1,2,3]   
+# messages = ["a", "b", "c"]
+# id_paxos = 0
 
 
-# TODO create e class called socket
-def mcast_sender():
-    """create a udp socket"""
-    send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    return send_sock
+# # TODO create e class called socket
+# def mcast_sender():
+#     """create a udp socket"""
+#     send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+#     return send_sock
 
-acceptors = ("239.0.0.1", 7000)
-for v in messages: 
+# acceptors = ("239.0.0.1", 7000)
+# for v in messages: 
 
     
-    for proposer in proposers:
-        socket_proposer = mcast_sender()
-        id_paxos = id_paxos + 1
-        instance_of_paxos = Paxos(id_paxos, proposer, socket_proposer)
-        instance_of_paxos.proposer_phase_1A(v, proposer)
+#     for proposer in proposers:
+#         socket_proposer = mcast_sender()
+#         id_paxos = id_paxos + 1
+#         instance_of_paxos = Paxos(id_paxos, proposer, socket_proposer)
+#         instance_of_paxos.proposer_phase_1A(v, proposer)
         
 
     
